543d800928247e0acdb53a841beb9662c6118343
glorp-fr <63018960+glorp-fr@users.noreply.github.com>
2025-11-28 14:29:49 +0100

    add files

---
+++ 03-terraform-vm.tf
@@ -0,0 +1,112 @@
+
+
+
+locals {
+  trigger = join("-", ["JTT", formatdate("YYYYMMDDhhmmss", timestamp())])
+  packer_init_splunk = terraform_data.packer_init_splunk.output
+  packer_init_http = terraform_data.packer_init_http.output
+  omi_delete = terraform_data.packer_build_splunk.output
+  keypair_name = "kp-splunk"
+}
+
+#############################################################################################################################
+#
+# Keypair
+#
+#############################################################################################################################
+
+resource "tls_private_key" "kp-splunk" {
+  algorithm = "RSA"
+  rsa_bits  = "2048"
+}
+
+resource "local_file" "kp-splunk" {
+  filename        = "${path.module}/kp-splunk.pem"
+  content         = tls_private_key.kp-splunk.private_key_pem
+  file_permission = "0600"
+}
+
+resource "outscale_keypair" "kp-splunk" {
+  keypair_name = "kp-splunk"
+  public_key = tls_private_key.kp-splunk.public_key_openssh
+}
+
+
+#############################################################################################################################
+#
+# Lancement de Packer splunk
+#
+#############################################################################################################################
+
+
+resource "terraform_data" "packer_init_splunk" {
+  input =  local.trigger
+
+  provisioner "local-exec" {
+    working_dir = "./"
+    command = "packer init vm_splunk.pkr.hcl" 
+  }
+}
+
+
+resource "terraform_data" "packer_build_splunk" {
+  input = local.packer_init_splunk
+  
+  provisioner "local-exec" {
+    working_dir = "./"
+    environment = {
+    OUTSCALE_ACCESSKEYID = "${var.access_key_id}"
+    OUTSCALE_SECRETKEYID = "${var.secret_key_id}"
+
+    }
+    command = "packer build vm_splunk.pkr.hcl" 
+  
+  }
+}
+
+
+data "outscale_images" "splunk" {
+  filter {
+   name = "image_names"
+   values = ["*splunk*"]
+  }
+  depends_on = [
+    terraform_data.packer_build_splunk
+  ]
+}
+
+#############################################################################################################################
+#
+# VM NET1 = Serveur Splunk
+#
+#############################################################################################################################
+
+resource "outscale_vm" "splunk" {
+    image_id  = tolist(data.outscale_images.splunk.images)[0].image_id
+    vm_type                  = "tinav7.c4r8p2"
+    keypair_name             = "kp-splunk"
+    subnet_id = outscale_subnet.splunk_net_sn1.subnet_id
+    security_group_ids = [outscale_security_group.splunk_net_sn1_sg.security_group_id]
+    tags {
+        key   = "name"
+        value = "splunk-server"
+    }
+    user_data                = base64encode(<<EOF
+    <CONFIGURATION>
+    EOF
+    )
+}
+
+resource "outscale_public_ip" "splunk_pub_ip"{
+	tags {
+	key="Name"
+	value="IP_Splunk"
+  }
+}
+
+
+
+resource "outscale_public_ip_link" "public_ip_link_splunk" {
+    vm_id     = outscale_vm.splunk.vm_id
+    public_ip = outscale_public_ip.splunk_pub_ip.public_ip
+}
---
+++ vm_splunk.hcl
@@ -0,0 +1,63 @@
+packer {
+  required_plugins {
+    outscale = {
+      version = ">= 1.5.0"
+      source  = "github.com/outscale/outscale"
+    }
+    ansible = {
+      source  = "github.com/hashicorp/ansible"
+      version = "~> 1"
+    }
+  }
+  required_version = ">= 1.7.0, < 2.0.0"
+}
+
+variable "region" {
+ type = string
+ default = "eu-west-2"
+}
+variable "omi_source" {
+  type =  string
+  default = "ami-38d543f0" # debian_12
+}
+
+locals {
+  timestamp = regex_replace(timestamp(), "[- TZ:]", "")
+}
+
+
+
+source "outscale-bsu" "debian_12" {
+  region = var.region
+  vm_type = "tinav7.c4r8p2"
+  ssh_username = "outscale"
+  communicator = "ssh"
+  ssh_interface = "public_ip"
+  #ssh_keypair_name = var.keypair_name
+  source_omi = var.omi_source
+  omi_name = "deb12_splunk"
+  #ssh_private_key_file = "./kp-splunk.pem"
+  launch_block_device_mappings {
+      delete_on_vm_deletion = true
+      device_name = "/dev/sda1"
+      volume_size = "40"
+      volume_type = "io1"
+      iops = "10000"
+  }
+}
+
+build {
+  name    = "deb12_splunk_jtt"
+  sources = ["source.outscale-bsu.debian_12"]
+
+  provisioner "shell" {
+    inline = [
+      "cloud-init status --wait",
+    ]
+  }
+  provisioner "ansible" {
+    playbook_file = "./Splunk_playbook.yml"
+    extra_arguments = [ "--scp-extra-args", "'-O'" ]
+  }
+}
+
