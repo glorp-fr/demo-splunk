6a6fc4cb96fbffd1ab9502805345a3c9b64a2b0e
glorp-fr <63018960+glorp-fr@users.noreply.github.com>
2025-11-28 16:23:39 +0100

    01-terraform_vlans

--- 01-terraform_vlans.tf
+++ 01-terraform_vlans.tf
@@ -1,21 +1,3 @@
-terraform {
-  required_providers {
-    outscale = {
-      source = "outscale/outscale"
-      version = "1.2.1"
-    }
-  }
-}  
-
-provider "outscale" {
-  access_key_id = var.access_key_id
-  secret_key_id = var.secret_key_id
-  region = var.region
-}
-
-output "path" { value= path.module}
-
-
 
 ##############################################################################################################################
 #
