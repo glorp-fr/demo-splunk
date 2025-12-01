769aa98add6b11516e0610403f5d0c275a708597
glorp-fr <63018960+glorp-fr@users.noreply.github.com>
2025-12-01 13:46:54 +0100

    test install.sh

--- install.sh
+++ install.sh
@@ -36,3 +36,4 @@ $SPLUNK_HOME/bin/splunk enable boot-start -user splunk
 echo "update rights on /opt/splunk"
 #Modification des droits sur le dossier de splunk:
 sudo chown splunk  /opt/splunk -R
+exit 0
