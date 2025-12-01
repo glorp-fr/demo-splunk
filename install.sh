113bcb3270191497d0869cad175e180d6c7874ef
glorp-fr <63018960+glorp-fr@users.noreply.github.com>
2025-12-01 11:43:23 +0100

    ansible

--- install.sh
+++ install.sh
@@ -11,16 +11,14 @@ export SPLUNK_HOME=/opt/splunk
 
 #récupération et installation des sources
 wget -O splunk-10.0.2-e2d18b4767e9-linux-amd64.deb "https://download.splunk.com/products/splunk/releases/10.0.2/linux/splunk-10.0.2-e2d18b4767e9-linux-amd64.deb"
-apt install -y ./splunk-10.0.2-e2d18b4767e9-linux-amd64.deb
+sudo apt install -y ./splunk-10.0.2-e2d18b4767e9-linux-amd64.deb
 
 #premier lancement de plunk
-$SPLUNK_HOME/bin/splunk start --no-prompt --answer-yes --accept-license
+sudo $SPLUNK_HOME/bin/splunk start --no-prompt --answer-yes --accept-license
 
-#Modification des droits sur le dossier de splunk:
-chown splunk  /opt/splunk -R
 
 #Arret 
-$SPLUNK_HOME/bin/splunk stop
+sudo $SPLUNK_HOME/bin/splunk stop
 
 #Création du user admin
 cat <<'EOF' > $SPLUNK_HOME/etc/system/local/user-seed.conf
@@ -32,3 +30,6 @@ EOF
 #Mise en place du démarrage du service splunk en automatique
 $SPLUNK_HOME/bin/splunk enable boot-start -user splunk
 
+
+#Modification des droits sur le dossier de splunk:
+sudo chown splunk  /opt/splunk -R
