cbcfd1c39c14b718b4ec04e0a0b42f9d706ea9a7
glorp-fr <63018960+glorp-fr@users.noreply.github.com>
2025-12-01 11:54:34 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ export SPLUNK_HOME=/opt/splunk
 wget -O splunk-10.0.2-e2d18b4767e9-linux-amd64.deb "https://download.splunk.com/products/splunk/releases/10.0.2/linux/splunk-10.0.2-e2d18b4767e9-linux-amd64.deb"
 sudo apt install -y ./splunk-10.0.2-e2d18b4767e9-linux-amd64.deb
 
+echo "Splunk install ok"
 #premier lancement de plunk
 sudo $SPLUNK_HOME/bin/splunk start --no-prompt --answer-yes --accept-license
 
@@ -20,6 +21,7 @@ sudo $SPLUNK_HOME/bin/splunk start --no-prompt --answer-yes --accept-license
 #Arret 
 sudo $SPLUNK_HOME/bin/splunk stop
 
+echo "admin user creation"
 #Création du user admin
 cat <<'EOF' > $SPLUNK_HOME/etc/system/local/user-seed.conf
 [user_info]
@@ -27,9 +29,10 @@ USERNAME = admin
 PASSWORD = InitPwd2025!
 EOF
 
+echo "auto start"
 #Mise en place du démarrage du service splunk en automatique
 $SPLUNK_HOME/bin/splunk enable boot-start -user splunk
 
-
+echo "update rights on /opt/splunk"
 #Modification des droits sur le dossier de splunk:
 sudo chown splunk  /opt/splunk -R
